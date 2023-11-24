e4e933b5ce69c2c6458e68afb79bcaf12ff0de9b
LucasC-B <lucascaputobello@gmail.com>
2023-11-24 16:42:14 -0300

    heroku atualizado

--- javascript/constantes.js
+++ javascript/constantes.js
@@ -1,3 +1,3 @@
-const backendAddress = 'http://127.0.0.1:8000/';
+const backendAddress = 'https://movietrackerbackend-51226b74db5e.herokuapp.com/';
 const tokenKeyword = 'Token ';
 const tokenKeyword2 = 'Bearer ';
